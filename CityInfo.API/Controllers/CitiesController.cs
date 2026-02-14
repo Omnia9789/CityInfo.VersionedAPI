1dffffe4bcc397c9efbd433203ac082824625b52
Omnia <Omnia@DESKTOP-Q2QA9HQ>
2026-02-14 20:02:37 +0200

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,94 @@
+﻿using Asp.Versioning;
+using AutoMapper;
+using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CityInfo.API.Controllers
+{
+    /// <summary>
+    /// Controller for managing cities and their points of interest.
+    /// </summary>
+    [ApiController]
+     [Authorize]
+    [Route("api/v{version:apiVersion}/cities")]
+    [ApiVersion(1)]
+    [ApiVersion(2)]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ICityInfoRepository _cityInfoRepository;
+        private readonly IMapper _mapper;
+        const int maxCitiesPageSize = 20;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CitiesController"/> class.
+        /// </summary>
+        /// <param name="cityInfoRepository">The repository for city information.</param>
+        /// <param name="mapper">The mapper for DTO conversions.</param>
+        public CitiesController(ICityInfoRepository cityInfoRepository,
+            IMapper mapper)
+        {
+            _cityInfoRepository = cityInfoRepository ??
+                throw new ArgumentNullException(nameof(cityInfoRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        /// <summary>
+        /// Retrieves a list of cities with optional filtering and pagination.
+        /// </summary>
+        /// <param name="name">The name of the city to filter by.</param>
+        /// <param name="searchQuery">The search query to filter cities.</param>
+        /// <param name="pageNumber">The page number for pagination.</param>
+        /// <param name="pageSize">The page size for pagination.</param>
+        /// <returns>A list of cities without points of interest.</returns>
+        [HttpGet]
+        [Produces("application/json")]
+
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(
+                    string? name, string? searchQuery, int pageNumber = 1, int pageSize = 10)
+        {
+            if (pageSize > maxCitiesPageSize)
+            {
+                pageSize = maxCitiesPageSize;
+            }
+
+            var (cityEntities, paginationMetadata) = await _cityInfoRepository
+                .GetCitiesAsync(name, searchQuery, pageNumber, pageSize);
+
+            Response.Headers.Add("X-Pagination",
+                JsonSerializer.Serialize(paginationMetadata));
+
+            return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
+        }
+
+        /// <summary>
+        /// Retrieves a city by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the city to retrieve.</param>
+        /// <param name="includePointsOfInterest">Whether to include points of interest in the response.</param>
+        /// <returns>The requested city with or without points of interest.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetCity(
+            int id, bool includePointsOfInterest = false)
+        {
+            var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            if (includePointsOfInterest)
+            {
+                return Ok(_mapper.Map<CityDto>(city));
+            }
+
+            return Ok(_mapper.Map<CityWithoutPointsOfInterestDto>(city));
+        }
+    }
+}
